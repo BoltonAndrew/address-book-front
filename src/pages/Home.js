f386891775f6541090142f16d0a8782cb638af02
BostonWafro <wafrogaming@gmail.com>
2021-02-10 14:21:51 +0000

    added router and pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,24 @@ import { ContactList } from '../components/ContactList';
 import { newContact } from '../utils';
 import { useState } from 'react';
  
-const Home = (newContact,) => {
+const Home = ({contactPath, contacts, newContactBool}) => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
-    const [contactList, setContactList] = useState({});
+    
 
     return(
         <>
-        {newContact && 
-            <form onSubmit={(event) => newContact(event, name, email, phone, contactList)}>
+        {newContactBool && 
+            <form onSubmit={(event) => newContact(event, name, email, phone, contacts)}>
                 <input type='text' placeholder='Name' onChange={(event) => setName(event.target.value)}/>
                 <input type='text' placeholder='Email' onChange={(event) => setEmail(event.target.value)}/>
                 <input type='text' placeholder='Phone' onChange={(event) => setPhone(event.target.value)}/>
                 <button>Add User</button>
             </form>}
-        <ContactList/>
+        <ContactList contactPath={contactPath}/>
         </>
     )
-}
+};
+
+export default Home;
